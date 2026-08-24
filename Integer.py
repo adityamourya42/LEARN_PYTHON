7941523df08d8fcaab68fb9ecc75070f506f6763
Mouryaditya Sunkara <155514226+adityamourya42@users.noreply.github.com>
2026-08-24 10:31:08 +0530

    Add files via upload

---
+++ Integer.py
@@ -0,0 +1,4 @@
+my_integer_var = 10
+print("Integer is a whole number without a decimals.")
+print("Integer:", my_integer_var)
+print(type(my_integer_var))
---
+++ float.py
@@ -0,0 +1,4 @@
+my_float_var = 4.50
+print("Float is a number with a decimal point.")
+print("Float:", my_float_var)
+print(type(my_float_var))
