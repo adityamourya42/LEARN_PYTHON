c76eb6fd92588a0096487690ffa5088d31174d0d
Mouryaditya Sunkara <155514226+adityamourya42@users.noreply.github.com>
2026-09-12 23:42:15 +0530

    Add files via upload

---
+++ scope.py
@@ -0,0 +1,7 @@
+tax_rate = 0.2
+def calculate_tax(price):
+    tax = price * tax_rate
+    return tax
+print(calculate_tax(54))
+print(tax_rate)
+##print(tax)
---
+++ sum.py
@@ -0,0 +1,9 @@
+def calculate_sum(a,b):
+    print(a+b)
+my_sum = calculate_sum(3,1)
+print(my_sum)
+
+def Multiply(a,b):
+    return a * b
+my_product = Multiply(5,15)
+print(my_product)
