51deca97074bf548f91d3df3d39ec35430692506
Mouryaditya Sunkara <155514226+adityamourya42@users.noreply.github.com>
2026-09-12 23:41:41 +0530

    Add files via upload

---
+++ Tuple1.py
@@ -0,0 +1,9 @@
+developer = ("Alice", 34, "Rust Developer")
+print(developer)
+print(type(developer))
+print(developer[1])
+print(developer[-1])
+print(len(developer))
+##print(developer[3])
+dev = "Jessica"
+print(tuple(dev))
---
+++ in-tuple.py
@@ -0,0 +1,5 @@
+developer = ("Alice", 34, "Java")
+name, age, job = developer
+print(name)
+print(age)
+print(job)
